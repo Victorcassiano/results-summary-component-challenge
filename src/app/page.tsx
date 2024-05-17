2136241fb6fa9389b4493b1b1ab03d882c13f026
Victor Cassiano <jose_victor@live.com>
2024-05-17 20:22:02 -0300

    Finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,117 @@
+"use client";
 import Image from "next/image";
+import {
+  SelectValue,
+  SelectTrigger,
+  SelectItem,
+  SelectGroup,
+  SelectContent,
+  Select,
+} from "@/components/ui/select";
+
+import TranslateJson from "../../public/assets/translate/index.json";
+import DataSummary from "../../public/assets/mock/data.json";
+import { useState } from "react";
+
+type TranslationKeys = {
+  title: string;
+  textPercent: string;
+  grade: string;
+  textButton: string;
+  scoredHigher: string;
+  summary: string;
+  Reaction: string;
+  Memory: string;
+  Verbal: string;
+  Visual: string;
+};
+
+type Translation = {
+  "en-US": TranslationKeys;
+  "pt-BR": TranslationKeys;
+};
 
 export default function Home() {
+  const [selectTranslate, setSelectTranslate] = useState<"en-US" | "pt-BR">(
+    "en-US"
+  );
+
+  const Translate: Translation = TranslateJson;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+    <main className="w-screen h-screen flex flex-col justify-center items-center bg-slate-200">
+      <div className="flex flex-row sm:flex-col bg-white w-[630px] sm:w-full h-[450px] sm:h-screen rounded-3xl sm:rounded-none overflow-hidden">
+        <section className="flex-1 flex flex-col gap-y-10 sm:py-5 sm:gap-y-5 items-center justify-center rounded-e-3xl sm:rounded-none sm:rounded-b-3xl bg-gradient-to-b from-[#755bfb] to-[#342fe5]">
+          <p className="text-slate-200/90 text-lg font-bold">Your Result</p>
+          <div className="flex flex-col gap-3 justify-center items-center w-40 h-40 rounded-full bg-gradient-to-b from-[#4b28c6] to-[#4b28c600]">
+            <p className="text-white font-bold text-6xl">76</p>
+            <span className="text-slate-200/90 font-medium">
+              {Translate[selectTranslate].textPercent} 100
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+          </div>
+          <div className="flex flex-col justify-center items-center px-8 gap-2">
+            <p className="text-white text-2xl font-bold">
+              {Translate[selectTranslate].grade}
+            </p>
+            <span className="text-slate-200/90 font-medium text-center">
+              {Translate[selectTranslate].scoredHigher}
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          </div>
+        </section>
+        <section className="flex-1">
+          <div className="flex flex-col px-8 py-8 gap-y-5 h-full">
+            <div className="flex flex-row items-center justify-between">
+              <p className="text-gray-700 font-bold text-2xl">
+                {Translate[selectTranslate].summary}
+              </p>
+              <Select
+                onValueChange={(translate: "en-US" | "pt-BR") =>
+                  setSelectTranslate(translate)
+                }
+              >
+                <SelectTrigger className="w-24">
+                  <SelectValue placeholder="en-US" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="en-US">en-US</SelectItem>
+                    <SelectItem value="pt-BR">pt-BR</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+            {DataSummary.map((item) => (
+              <section
+                key={item.category}
+                style={{ backgroundColor: item.bg }}
+                className={`flex flex-row items-center justify-between p-4 w-full h-12 rounded-lg`}
+              >
+                <div className="flex flex-row items-center gap-3">
+                  <Image
+                    src={item.icon}
+                    alt={item.category}
+                    className="w-6 h-6"
+                    width={50}
+                    height={50}
+                  />
+                  <p className={`font-bold`} style={{ color: item.textColor }}>
+                    {Translate[selectTranslate][item.category]}
+                  </p>
+                </div>
+                <div className="flex flex-row items-center gap-3 font-bold">
+                  <p className="text-gray-800">
+                    {item.score}{" "}
+                    <span className="text-gray-700/60"> / 100</span>
+                  </p>
+                </div>
+              </section>
+            ))}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <button className="bg-gray-800 w-full h-12 rounded-3xl font-bold text-white">
+              Continue
+            </button>
+          </div>
+        </section>
       </div>
     </main>
   );
